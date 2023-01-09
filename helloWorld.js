ad9efe749d85412a2fa91a0c0f1bcb2ae8a1a75f
vasilsafronov <122278841+vasilsafronov@users.noreply.github.com>
2023-01-09 12:38:13 +0000

    Add files via upload

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+/* To write a program that prints out the message “Hello World!” */
+
+// console.log("Hello World");
+// console.log("Git is Awesome!");
+
+// Now the program accepts input from the user and then prints out the inputted data
+
+let inputUser = prompt("Please enter your name: ");
+
+console.log(inputUser);
+
