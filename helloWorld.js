825cc596f13e2c80da91f577cae3c9ca7f961711
Vasilii Safronov <safronov@taurus-management.com>
2023-01-09 12:17:15 +0000

    Now the program accepts input from the user and then prints out the inputted data

--- helloWorld.js
+++ helloWorld.js
@@ -3,7 +3,7 @@
 // console.log("Hello World");
 // console.log("Git is Awesome!");
 
-// Now the program to accept input from the user and then print out the inputted data
+// Now the program accepts input from the user and then prints out the inputted data
 
 let inputUser = prompt("Please enter your name: ");
 
